54bd6f0e0f212bf2adb553633ce87627fafe9edf
Inexperienced Me <inexperiencedme@protonmail.com>
2025-04-04 20:28:41 +0200

    added kualtas .md files with my metadata

---
+++ utils/getPostsMetadata.js
@@ -0,0 +1,29 @@
+
+
+function getPostsMetadata() {
+  const postsPath = path.join(process.cwd(), "/posts/");
+  const files = readdirSync(postsPath);
+  const markdownFiles = files.filter((file) => file.endsWith(".md"));
+
+  const posts = markdownFiles
+    .map((file) => {
+      const fileContents = readFileSync(`${postsPath}${file}`, "utf-8");
+      const metadata = matter(fileContents);
+      const filename = file.replace(".md", "");
+
+      return {
+        title: metadata.data.title,
+        description: metadata.data.description,
+        date: metadata.data.date,
+        tags: metadata.data.tags,
+        preview: metadata.data.preview,
+        filename: filename};})
+    .sort((a, b) => {
+      const first = Date.parse(a.date);
+      const second = Date.parse(b.date);
+      return second - first;});
+
+  return posts;
+}
+
+export default getPostsMetadata;
