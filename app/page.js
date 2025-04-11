bda6dc2b07e3f15d72eda30c7767e98261efd2a8
Inexperienced Me <inexperienceme@protonmail.com>
2025-04-12 01:54:31 +0200

    big style changes, responsive main text, better header spacing n stuff

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 async function HomePage() {
   return (
     <div className={"flex flex-col w-full grow justify-center font-bold"}>
-        <h1 className="text-8xl text-center">
+        <h1 className="absolute top-1/3 w-full text-center text-[10vw]">
           Inexperienced Me
         </h1>
     </div>
