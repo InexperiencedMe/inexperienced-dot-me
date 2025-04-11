c195c80925bc9013142a6c6c7cbe6a11aa709a71
Inexperienced Me <inexperiencedme@protonmail.com>
2025-04-11 15:10:54 +0200

    dreams blog post added

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -13,10 +13,10 @@ export const generateStaticParams = async () => {
 function PostContent({ post }) {
   return (
     <article className={`p-2 py-8 prose prose-md md:prose-lg lg:prose-xl dark:prose-invert prose-img:rounded-lg
-                         prose-p:text-justify prose-em:opacity-40 prose-img:w-full rose-a:text-cyan-500
+                         prose-p:text-justify prose-em:opacity-40 prose-img:w-full prose-a:text-cyan-500
                          prose-a:no-underline prose-a:hover:underline prose-headings:`}>
-      <h1 className="mb-2 text-center">{post.data.title}</h1>
-      <h3 className="mb-6 mt-0 text-center">{post.data.subtitle}</h3>
+      <h1 className="!mb-0 text-center">{post.data.title}</h1>
+      <h3 className="!mb-8 !mt-2 text-center">{post.data.subtitle}</h3>
       <Markdown options={{overrides: {VideoEmbed: VideoEmbed}}}>
         {post.content}
       </Markdown>
