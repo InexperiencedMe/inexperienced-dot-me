ce137df0ab51cc2a1a55f1bc7b4bbb0895b8e8d6
Inexperienced Me <inexperienceme@protonmail.com>
2025-04-12 14:05:30 +0200

    improved background colors, cards, styling

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,7 @@
 export const Card = (props) => {
   return (
-    <div className="p-2 rounded-md border-2 hover:border-accent hover:transition-colors">
+    <div className="p-2 bg-background-secondary rounded-md border-2 border-background
+                    hover:border-accent hover:transition-colors duration-100 drop-shadow-2xl">
       {props.children}
     </div>
   );
