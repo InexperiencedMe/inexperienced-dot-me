4a6f7950b910db6babcee9b6891c660c4f2dd546
Inexperienced Me <inexperienceme@protonmail.com>
2025-04-12 22:49:12 +0200

    better responsive main page

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ async function HomePage() {
   return (
     <div className={"flex flex-col w-full grow justify-center font-bold"}>
       <div className="flex flex-col gap-2 mx-auto">
-        <h1 className="relative w-full text-center text-[10vw]">
+        <h1 className="relative w-full text-center text-[clamp(3rem,12vw,6rem)]">
           Inexperienced Me
         </h1>
-        <h2 className="relative w-full text-center text-[4vw]">
+        <h2 className="relative w-full text-center text-[clamp(1rem,4vw,2rem)]">
           One-man intelligence agency
         </h2>
       </div>
