92eeefc946f5b58ef1403efec446424f927068d5
Inexperienced Me <inexperiencedme@protonmail.com>
2025-04-12 15:26:06 +0200

    light mode flickering fixed, but kinda ugly

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,21 @@ export default function RootLayout({children}) {
     <html lang="en" className="scroll-smooth">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  const storedTheme = localStorage.getItem('theme');
+                  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                  
+                  if (storedTheme === 'dark' || (!storedTheme && prefersDark)) {
+                    document.documentElement.classList.add('dark');
+                  }
+                } catch (err) { }
+              })()`
+          }}
+        />
       </head>
       <body
         className={`bg-background text-foreground place-items-center min-h-screen flex flex-col relative ${fredoka.className}`}>
