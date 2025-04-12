dd34ffd7b9d4eaf55bea52640dfbe34acf67c35c
Inexperienced Me <inexperienceme@protonmail.com>
2025-04-12 15:33:21 +0200

    preventing flickering a bit cleaner

--- app/layout.js
+++ app/layout.js
@@ -12,21 +12,13 @@ export default function RootLayout({children}) {
     <html lang="en" className="scroll-smooth">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                try {
-                  const storedTheme = localStorage.getItem('theme');
-                  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-                  
-                  if (storedTheme === 'dark' || (!storedTheme && prefersDark)) {
-                    document.documentElement.classList.add('dark');
-                  }
-                } catch (err) { }
-              })()`
-          }}
-        />
+        <script dangerouslySetInnerHTML={{
+          __html: // Preventing flickering of light mode on refresh
+            `if (localStorage.getItem('dark-mode') === 'true' || (!('dark-mode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+              document.querySelector('html').classList.add('dark');
+            } else {
+                document.querySelector('html').classList.remove('dark');
+            }`}}/>
       </head>
       <body
         className={`bg-background text-foreground place-items-center min-h-screen flex flex-col relative ${fredoka.className}`}>
