4de9adec7fc32565b61eab70fa4515cd2f838109
Inexperienced Me <inexperiencedme@protonmail.com>
2025-04-14 01:48:45 +0200

    good safe improvements, fixed className, icon size, heading spacing

--- components/Header.js
+++ components/Header.js
@@ -5,13 +5,13 @@ export const Header = () => {
   return (
     <>
       <div className={`z-[100] flex flex-row place-content-between
-      text-xl md:text-2xl lg:text-3xl max-w-full w-full h-fit mt-4 mb-8 px-4`}>
+      text-2xl lg:text-3xl max-w-full w-full h-fit mt-4 mb-8 px-4`}>
         
         <div className="flex">
           <BackButton />
         </div>
 
-        <div className="flex gap-x-4 md:gap-x-16">
+        <div className="flex gap-x-6 md:gap-x-16">
           <Link href="/blog">     blog    </Link>
           <Link href="/about">    about   </Link>
           <Link href="/contact">  contact </Link>
