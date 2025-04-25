68611f09ddf94fb2dfbdb4fe8b4c024121adb041
Inexperienced Me <inexperienceme@protonmail.com>
2025-04-25 20:59:39 +0200

    fixed card jitter, no gradient but nice blendin

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,9 @@
 export const Card = (props) => {
   return (
     <div className="p-2 bg-background-secondary rounded-md border-2 border-background-secondary 
+                  hover:bg-background transition-all duration-300 ease-in-out
                   relative overflow-hidden hover:border-foreground-secondary drop-shadow-xl group
-                  hover:scale-102 transition-transform duration-300 ease-in-out">
-      <div className="absolute inset-0 opacity-0 group-hover:opacity-10 transition-opacity duration-300 ease-in-out 
-                    bg-gradient-to-br from-foreground-secondary via-30% via-foreground-secondary to-transparent pointer-events-none"></div>
+                  hover:scale-104 transform-gpu will-change-transform">
       {props.children}
     </div>
   );
