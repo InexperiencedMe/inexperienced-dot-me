d931869ba8490b0324d6e59f10d83b100dd07a1c
Inexperienced Me <inexperiencedme@protonmail.com>
2025-12-07 22:47:39 +0100

    fixed maybe, please

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { Header } from "@/components/Header";
 import { fredoka } from "@/styles/fonts";
-import { unstable_ViewTransition as ViewTransition } from "react";
+import { ViewTransition } from "react";
 
 export const metadata = {
   title: "Inexperienced Me",
--- next.config.mjs
+++ next.config.mjs
@@ -4,32 +4,49 @@ const nextConfig = {
     viewTransition: true,
   },
 
+  // 1. Keep Webpack for "next build"
   webpack(config) {
-    // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.('.svg')
     );
 
     config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
+        resourceQuery: /url/, 
       },
-      // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, 
         use: ['@svgr/webpack'],
       }
     );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
-
     return config;
+  },
+
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              // This option forces the default export to be the component
+              // preventing the "undefined" error.
+              exportType: 'default', 
+              ref: true,
+              svgo: true,
+              titleProp: true,
+            },
+          },
+        ],
+        as: '*.js',
+      },
+    },
   },
 };
 
